4090d2a42c4529193b84bb3e6482d0defccf61ee
Douglas Viana <douglas.rfvianna@gmail.com>
2021-11-18 03:24:28 -0300

    deletando arquivos

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
 
-// import AuthForm from './Components/AuthForm/AuthForm';
-
 
 ReactDOM.render(
   <React.StrictMode>
